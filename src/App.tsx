36d91960aa9fba8df5cdfb5e20d8a9985af92d49
Ricardo Rosa <ricardo.rosa@devtest.co.ao>
2025-07-10 15:28:53 +0100

    Corrige rotas duplicadas e remove aninhamento inválido

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-// App.tsx
+// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home'; // funciona se o arquivo for "home.tsx"
+import Home from './pages/Home';
+// Importe aqui os outros componentes/rotas quando estiverem prontos
+// import About from './pages/About';
+// import Game from './pages/Game';
 
-
-
-const App: React.FC = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         {/* Adicione outras rotas aqui, por exemplo: */}
-        {/* <Route path="/play" element={<Play />} /> */}
-        {/* <Route path="/tournaments" element={<Tournaments />} /> */}
-        {/* <Route path="/profile" element={<Profile />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/game" element={<Game />} /> */}
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
